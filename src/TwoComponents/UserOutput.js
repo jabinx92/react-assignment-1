6193f06e2e4778f0ca1c80e09a4cec837432c33f
Jonathan Won <jabinx92@gmail.com>
2020-06-05 00:38:45 -0700

    gn

---
+++ src/TwoComponents/UserOutput.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function UserOutput () {
+    return (
+        <div>
+            <p>Hello, my name is Johnny, I have been studying to be a software engineer for a year now.</p>
+
+            <p>Johnny is passionate for video games and technology and coding.</p>
+        </div>
+    )
+}
+
+export default UserOutput
